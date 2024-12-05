4678f3dc67c9872a542e904d4f88402a46cca93d
fikaaw <125174361+Fikaaw@users.noreply.github.com>
2024-12-05 09:48:20 +0700

    path fixing

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -58,8 +58,8 @@ def create_byweather_df(df):
     return weather_df
 
 # Load cleaned data
-day_clean_df = pd.read_csv(r"dashboard/bike_day.csv")
-hour_df = pd.read_csv(r"dataset/hour.csv")
+day_clean_df = pd.read_csv("dashboard/bike_day.csv")
+hour_df = pd.read_csv("dataset/hour.csv")
 
 # Filter data
 day_clean_df["dteday"] = pd.to_datetime(day_clean_df["dteday"])
@@ -69,7 +69,7 @@ max_date = day_clean_df["dteday"].max()
 
 with st.sidebar:
     # Menambahkan logo 
-    st.image(r"dashboard/logo.jpg")
+    st.image("dashboard/logo.jpg")
     
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
